c9a50cbc40f367c1e338e4ead49bb89e7c6fd04a
Aryan Kushwaha <59092136+funinkina@users.noreply.github.com>
2026-06-11 20:47:23 +0530

    update icon

--- gui/src/about.rs
+++ gui/src/about.rs
@@ -5,7 +5,7 @@ use adw::prelude::*;
 pub fn present(parent: &impl IsA<gtk::Widget>) {
     let about = adw::AboutDialog::builder()
         .application_name("OpenEffects")
-        .application_icon("camera-web-symbolic")
+        .application_icon("openeffects")
         .version(env!("CARGO_PKG_VERSION"))
         .comments("Linux-native webcam effects engine")
         .developer_name("Aryan Kushwaha")
